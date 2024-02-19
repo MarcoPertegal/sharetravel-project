9d6f2aa1b0e5bffd95ac7e2b7d4af2d62d3653c5
Marco <marco.pertegal.prieto@gmail.com>
2024-02-19 17:49:36 +0100

    Base proyects

---
+++ sharetravelBackend/src/main/java/com/salesianostriana/dam/sharetravelBackend/SharetravelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.sharetravelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharetravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharetravelBackendApplication.class, args);
+	}
+
+}
---
+++ sharetravel_frontend/android/app/src/main/kotlin/com/example/sharetravel_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sharetravel_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
