28a7e7b9a58ee139c5a1426ab1252798ff728bc5
Marco <marco.pertegal.prieto@gmail.com>
2024-06-02 16:52:10 +0200

    Adding filter trip request, html form and modify trip ts to manage trip filter list and trip list requests

--- sharetravel_management/src/app/ui/trip-page/trip-page.component.ts
+++ sharetravel_management/src/app/ui/trip-page/trip-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { Trip } from '../../model/get-all-trips-response.interface';
+
 import { TripService } from '../../service/trip.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { GetTripByIDResponse } from '../../model/get-trip-by-id.interface';
-
 @Component({
   selector: 'app-trip-page',
   templateUrl: './trip-page.component.html',
@@ -15,27 +15,36 @@ export class TripPageComponent {
   pageNumber: number = 0;
   count: number = 0;
   tripId!: string;
+  isFiltered: boolean = false;
 
   constructor(
     private tripService: TripService,
     private modalService: NgbModal
-  ) {
-  }
+  ) { }
+
   ngOnInit(): void {
     this.loadNewPage();
   }
 
   loadNewPage() {
-    this.tripService.GetAll(this.pageNumber - 1).subscribe(resp => {
-      this.tripList = resp.content;
-      this.count = resp.totalElements;
-    });
+    if (this.isFiltered) {
+      this.tripService.filterTrip(
+        this.findTrip.value.departurePlace!,
+        this.findTrip.value.arrivalPlace!,
+        this.findTrip.value.departureDate!,
+        this.pageNumber - 1
+      ).subscribe(resp => {
+        this.tripList = resp.content;
+        this.count = resp.totalElements;
+      });
+    } else {
+      this.tripService.GetAll(this.pageNumber - 1).subscribe(resp => {
+        this.tripList = resp.content;
+        this.count = resp.totalElements;
+      });
+    }
   }
 
-  ////////////////EDIT TRIP
-  //metodo para abrir el modal para abrirlo simplemente hace 
-  //falta el "this.modalService.open(editTrip);" y el "this.tripId = id;" para pasarselo
-  //al metodo que edita el viaje el resto es para poner los datos del viaje en el formulario
   editModal(editTrip: any, id: any) {
     this.tripId = id;
     this.tripService.getTripById(id).subscribe(
@@ -67,7 +76,7 @@ export class TripPageComponent {
     estimatedDuration: new FormControl('', [Validators.required, Validators.min(3)]),
     price: new FormControl('', [Validators.required, Validators.min(1)]),
     tripDescription: new FormControl('', Validators.required),
-  })
+  });
 
   editTr() {
     this.tripService.editTripById(
@@ -93,9 +102,9 @@ export class TripPageComponent {
         } else {
           window.alert('Something go wrong!!');
         }
-      }
-    );
+      });
   }
+
   closeModal() {
     this.modalService.dismissAll();
   }
@@ -119,4 +128,41 @@ export class TripPageComponent {
     this.modalService.open(deleteTrip);
   }
 
+  filterModal(filterTrip: any) {
+    this.modalService.open(filterTrip);
+  }
+
+  findTrip = new FormGroup({
+    departurePlace: new FormControl('', Validators.required),
+    arrivalPlace: new FormControl('', Validators.required),
+    departureDate: new FormControl('', Validators.required)
+  });
+
+  findTr() {
+    this.tripService.filterTrip(
+      this.findTrip.value.departurePlace!,
+      this.findTrip.value.arrivalPlace!,
+      this.findTrip.value.departureDate!,
+      this.pageNumber - 1
+    ).subscribe(resp => {
+      this.tripList = resp.content;
+      this.count = resp.totalElements;
+      this.isFiltered = true;
+      this.closeModal();
+    },
+      error => {
+        if (error.status === 400) {
+          if (error.error.errors && error.error.errors.length > 0) {
+            const errorMessage = error.error.errors[0].defaultMessage;
+            window.alert(errorMessage);
+          } else {
+            window.alert('Invalid date time format');
+          }
+        } else if (error.status === 404) {
+          window.alert(error.error.message);
+        } else {
+          window.alert('Something go wrong!!');
+        }
+      });
+  }
 }
